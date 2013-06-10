06bfa7ef26aa1633ffd9d79e350a1d3f32bcf2df
Henning Stoverud <henning@stoverud.no>
2013-06-10 10:43:12 +0200

    Some code cleanup. Moved stuff into seperate modules etc..

---
+++ ardrone-dashboard/ardrone-module.js
@@ -0,0 +1,12 @@
+exports.blabla = function(client) {
+
+		console.log('Executing flight program');
+
+		client.animateLeds('blinkGreenRed', 5, 10);
+		client.takeoff();
+		client
+		  .after(1000, function() {
+		    this.stop();
+		    this.land();
+		  });
+}
---
+++ ardrone-dashboard/public/js/charting.js
@@ -0,0 +1,182 @@
+// Add highcharts to the view
+var altitude_chart;
+var speed_graph;
+$(function () {
+	
+    speed_chart = new Highcharts.Chart({
+	
+	    chart: {
+	    	renderTo: 'speed_gauge',
+	        type: 'gauge',
+	        plotBackgroundColor: null,
+	        plotBackgroundImage: null,
+	        plotBorderWidth: 0,
+	        plotShadow: false
+	    },
+	    
+	    title: {
+	        text: 'Speedometer'
+	    },
+	    
+	    pane: {
+	        startAngle: -150,
+	        endAngle: 150,
+	        background: [{
+	            backgroundColor: {
+	                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	                stops: [
+	                    [0, '#FFF'],
+	                    [1, '#333']
+	                ]
+	            },
+	            borderWidth: 0,
+	            outerRadius: '109%'
+	        }, {
+	            backgroundColor: {
+	                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	                stops: [
+	                    [0, '#333'],
+	                    [1, '#FFF']
+	                ]
+	            },
+	            borderWidth: 1,
+	            outerRadius: '107%'
+	        }, {
+	            // default background
+	        }, {
+	            backgroundColor: '#DDD',
+	            borderWidth: 0,
+	            outerRadius: '105%',
+	            innerRadius: '103%'
+	        }]
+	    },
+	       
+	    // the value axis
+	    yAxis: {
+	        min: 0,
+	        max: 200,
+	        
+	        minorTickInterval: 'auto',
+	        minorTickWidth: 1,
+	        minorTickLength: 10,
+	        minorTickPosition: 'inside',
+	        minorTickColor: '#666',
+	
+	        tickPixelInterval: 30,
+	        tickWidth: 2,
+	        tickPosition: 'inside',
+	        tickLength: 10,
+	        tickColor: '#666',
+	        labels: {
+	            step: 2,
+	            rotation: 'auto'
+	        },
+	        title: {
+	            text: 'km/h'
+	        },
+	        plotBands: [{
+	            from: 0,
+	            to: 120,
+	            color: '#55BF3B' // green
+	        }, {
+	            from: 120,
+	            to: 160,
+	            color: '#DDDF0D' // yellow
+	        }, {
+	            from: 160,
+	            to: 200,
+	            color: '#DF5353' // red
+	        }]        
+	    },
+	
+	    series: [{
+	        name: 'Speed',
+	        data: [0],
+	        tooltip: {
+	            valueSuffix: ' km/h'
+	        }
+	    }],
+        exporting: {
+            enabled: false
+        }
+
+	
+	} 
+	);
+
+
+	Highcharts.setOptions({
+		global : {
+			useUTC : false
+		}
+	});
+
+		altitude_chart = new Highcharts.Chart({
+		plotOptions: {
+			series: {
+				animation: false
+			},
+			line: {
+				animation: false
+			},
+			area: {
+				marker: {
+					enabled: false
+				}
+			}
+		},
+    chart: {
+    	renderTo: 'altitude_chart',
+        type: 'area',
+        animation: Highcharts.svg, // don't animate in old IE
+        marginRight: 10
+    },
+    title: {
+        text: 'Altitude'
+    },
+    xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150
+    },
+    yAxis: {
+    	minRange: 0,
+        title: {
+            text: 'Meters'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
+    },
+    tooltip: {
+        formatter: function() {
+                return '<b>'+ this.series.name +'</b><br/>'+
+                Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                Highcharts.numberFormat(this.y, 2);
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    exporting: {
+        enabled: false
+    },
+    series: [{
+        name: 'Random data',
+         data: (function() {
+                // generate an array of random data
+                var data = [],
+                    time = (new Date()).getTime(),
+                    i;
+
+                for (i = -19; i <= 0; i++) {
+                    data.push({
+                        x: time + i * 1000,
+                        y: 0
+                    });
+                }
+                return data;
+            })()	            }]
+	});
+});
